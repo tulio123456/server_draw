2d96ac13e29ffcac9cf7a4672be09e47d63acfcb
tulio123456 <122922547+tulio123456@users.noreply.github.com>
2026-08-08 16:08:04 -0300

    Delete api directory

--- api/admin/me.js
+++
@@ -1,8 +0,0 @@
-import { isAdmin } from "../../lib/auth.js";
-import { json } from "../../lib/http.js";
-
-export function GET(request) {
-  return json({
-    authenticated: isAdmin(request)
-  });
-}
